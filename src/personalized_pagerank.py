5fe8d155228687a2aa35803f03bb25ce74f0c94c
Jason Kepler <jasonkepler1@gmail.com>
2023-08-21 13:28:03 -0700

    Cleanup

--- src/personalized_pagerank.py
+++ src/personalized_pagerank.py
@@ -2,8 +2,7 @@ import networkx as nx
 
 
 ALPHA = 0.85
-MAX_ITER = 1000 # raises error if reached
-TOL = 1e-01
+TOL = 1e-01 # higher tolerance for faster convergence
 
 
 def initialize(G, directed):
@@ -23,11 +22,12 @@ def predict(G, directed):
     # run personalized pagerank for each node
     for node in G.nodes():
         personalization = {node: 1}
-        pr_scores = nx.pagerank(G, alpha=ALPHA, personalization=personalization, max_iter=MAX_ITER, tol=TOL)
+        pr_scores = nx.pagerank(G, alpha=ALPHA, personalization=personalization, tol=TOL)
 
         # sort nodes by pagerank score
         sorted_nodes = sorted(pr_scores.items(), key=lambda x: x[1], reverse=True)
 
+        # find the top edge that doesn't already exist
         for predicted_node, score in sorted_nodes:
             predicted_node = int(predicted_node)
             if not tmp_g.has_edge(node, predicted_node):
