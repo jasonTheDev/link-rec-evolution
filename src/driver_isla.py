526edb3ab48a2540d682476708d6d133d316918d
Alex Thomo <thomo@uvic.ca>
2023-12-24 11:08:44 -0800

    Just a commit test

--- src/driver_isla.py
+++ src/driver_isla.py
@@ -9,6 +9,8 @@ import math
 # local imports
 from utils.metrics import Recorder
 
+# Hi
+
 # methods
 WAGNER = "from methods import Wagner2022 as Method"
 OTHERMETHOD = "from methods import OtherMethod as Method" # just an example for now
