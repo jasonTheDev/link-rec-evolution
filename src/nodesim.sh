825118c010dcf621ebf42d39b9109e5bbac8b070
Alex Thomo <thomoal@gmail.com>
2024-01-13 11:21:04 -0800

    more data files for 15% for crosswalk and nodesim diff params

--- src/nodesim.sh
+++ src/nodesim.sh
@@ -1,6 +1,15 @@
-python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 1.0 --param2 2.0
+python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 2.0 --param2 2.0
 
-python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 1.0 --param2 3.0
+python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 2.0 --param2 3.0
 
-python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 1.0 --param2 4.0
+python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 2.0 --param2 4.0
+
+
+python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 3.0 --param2 2.0
+
+python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 3.0 --param2 3.0
+
+python3 ./driver_isla.py --minority_percentages 15percent --algorithms nodesim --datasets facebook,False --param1 3.0 --param2 4.0
+
+
 
